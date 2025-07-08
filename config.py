92285c6f2fd375a1050d76bf5257dcd380ecd5d6
komiljanov2101 <abdulazizbek21012010@gmail.com>
2025-07-08 08:38:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-# config.py
+import os
 
-BOT_TOKEN = "7739117900:AAHjKGVqeJR2PToZ_1p2mb9ndVuR-nMr-xY"
-ADMIN_IDS = [5111095291, 5454891351]  # O'zingiz istagan adminlar ID sini yozing
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Admin ID larni string ko'rinishidan list[int] ga aylantiramiz
+ADMIN_IDS = [int(admin_id.strip()) for admin_id in os.getenv("ADMIN_IDS", "").split(",") if admin_id.strip()]
